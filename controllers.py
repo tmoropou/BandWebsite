21df2c84353cda6fb42ad9b1e9e2874308c983e4
tmoropou <63266709+tmoropou@users.noreply.github.com>
2021-04-28 17:37:24 -0700

    init commit

---
+++ controllers.py
@@ -0,0 +1,53 @@
+"""
+This file defines actions, i.e. functions the URLs are mapped into
+The @action(path) decorator exposed the function at URL:
+
+    http://127.0.0.1:8000/{app_name}/{path}
+
+If app_name == '_default' then simply
+
+    http://127.0.0.1:8000/{path}
+
+If path == 'index' it can be omitted:
+
+    http://127.0.0.1:8000/
+
+The path follows the bottlepy syntax.
+
+@action.uses('generic.html')  indicates that the action uses the generic.html template
+@action.uses(session)         indicates that the action uses the session
+@action.uses(db)              indicates that the action uses the db
+@action.uses(T)               indicates that the action uses the i18n & pluralization
+@action.uses(auth.user)       indicates that the action requires a logged in user
+@action.uses(auth)            indicates that the action requires the auth object
+
+session, db, T, auth, and tempates are examples of Fixtures.
+Warning: Fixtures MUST be declared with @action.uses({fixtures}) else your app will result in undefined behavior
+"""
+
+from py4web import action, request, abort, redirect, URL
+from yatl.helpers import A
+from .common import db, session, T, cache, auth, logger, authenticated, unauthenticated, flash
+from py4web.utils.url_signer import URLSigner
+from .models import get_user_email
+
+url_signer = URLSigner(session)
+
+@action('index')
+@action.uses(db, auth, 'index.html')
+def index():
+    ## TODO: Show to each logged in user the birds they have seen with their count.
+    # The table must have an edit button to edit a row, and also, a +1 button to increase the count
+    # by 1 (this needs to be protected by a signed URL).
+    # On top of the table there is a button to insert a new bird.
+    return dict()
+
+# This is an example only, to be used as inspiration for your code to increment the bird count.
+# Note that the bird_id parameter ...
+@action('capitalize/<bird_id:int>') # the :int means: please convert this to an int.
+@action.uses(db, auth.user, url_signer.verify())
+# ... has to match the bird_id parameter of the Python function here.
+def capitalize(bird_id=None):
+    assert bird_id is not None
+    bird = db.bird[bird_id]
+    db(db.bird.id == bird_id).update(bird_name=bird.bird_name.capitalize())
---
+++ models.py
@@ -0,0 +1,30 @@
+"""
+This file defines the database models
+"""
+
+import datetime
+from .common import db, Field, auth
+from pydal.validators import *
+
+
+def get_user_email():
+    return auth.current_user.get('email') if auth.current_user else None
+
+def get_time():
+    return datetime.datetime.utcnow()
+
+
+### Define your table below
+#
+# db.define_table('thing', Field('name'))
+#
+## always commit your models to avoid problems later
+
+db.define_table(
+    'bird',
+    ### TODO: define the fields that are in the json.
+    Field('bird_count', 'integer'),
+    Field('seen_by', default=get_user_email()),
+)
+
+db.commit()
