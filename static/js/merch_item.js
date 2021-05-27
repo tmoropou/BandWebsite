95af887b317dd9fed4cbcf49af275042ef28423a
maxnibler <max.nibler@gmail.com>
2021-05-27 15:06:37 -0700

    merch item page frontend

--- static/js/merch_item.js
+++ static/js/merch_item.js
@@ -9,6 +9,9 @@ let init = (app) => {
     // This is the Vue data.
     app.data = {
         rows: [],
+        stars: 5,
+        write_review: false,
+        review_body: "",
         logged_in: false,
         item: {}
     };
@@ -20,6 +23,20 @@ let init = (app) => {
         return a;
     };
 
+    app.toggle_review = function () {
+        app.vue.write_review = !app.vue.write_review;
+        app.vue.review_body = "";
+    }
+
+    app.set_star = function (count) {
+        app.vue.stars = count;
+    }
+
+    app.submit_review = function () {
+        console.log(app.vue.stars, app.vue.review_body);
+        app.toggle_review();
+    }
+
     app.check_logged = () => {
         if(global_email != "None"){
             app.vue.logged_in = true;
@@ -29,6 +46,9 @@ let init = (app) => {
     }
 
     app.methods = {
+        toggle_review: app.toggle_review,
+        set_star: app.set_star,
+        submit_review: app.submit_review,
     };
 
     // This creates the Vue instance.
