0fe3ade04cd83207fdcf97972f7b731b867cfa7e
tmoropou <63266709+tmoropou@users.noreply.github.com>
2021-05-26 22:01:39 -0700

    Setting up show or hide sign in button if user is logged in

---
+++ static/js/layout.js
@@ -0,0 +1,48 @@
+// This will be the object that will contain the Vue attributes
+// and be used to initialize it.
+let app = {};
+
+// Given an empty app object, initializes it filling its attributes,
+// creates a Vue instance, and then initializes the Vue instance.
+let init = (app) => {
+
+    // This is the Vue data.
+    app.data = {
+        logged_in: true,
+        not_logged_in: false,
+    };
+
+    app.enumerate = (a) => {
+        // This adds an _idx field to each element of the array.
+        let k = 0;
+        a.map((e) => {e._idx = k++;});
+        return a;
+    };
+
+    app.methods = {
+    };
+
+    // This creates the Vue instance.
+    app.vue = new Vue({
+        el: "#vue-target",
+        data: app.data,
+        methods: app.methods
+    });
+
+    app.init = () => {
+        // Do any initializations (e.g. networks calls) here.
+//        axios.get(check_admin_url).then(function (response) {
+//            if (response.data.admin > 0){
+//                app.vue.admin_flag = true;
+//            }
+//        });
+        // app.create_user();
+    };
+
+    // Call to the initializer.
+    app.init();
+};
+
+// This takes the (empty) app object, and initializes it,
+// putting all the code i
+init(app);
