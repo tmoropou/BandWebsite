c04d425eae6eac4281701ea59e1520f75582c40f
maxnibler <max.nibler@gmail.com>
2021-05-30 13:59:37 -0700

    begining setup for google host

--- settings.py
+++ settings.py
@@ -56,9 +56,6 @@ LOGGERS = [
     "warning:stdout"
 ]  # syntax "severity:filename" filename can be stderr or stdout
 
-# single sign on Google (will be used if provided)
-OAUTH2GOOGLE_CLIENT_ID = None
-OAUTH2GOOGLE_CLIENT_SECRET = None
 
 # single sign on Okta (will be used if provided. Please also add your tenant
 # name to py4web/utils/auth_plugins/oauth2okta.py. You can replace the XXX
@@ -90,6 +87,6 @@ CELERY_BROKER = "redis://localhost:6379/0"
 
 # try import private settings
 try:
-    from .settings_private import *
+    from .private.secret_settings import *
 except (ImportError, ModuleNotFoundError):
     pass
